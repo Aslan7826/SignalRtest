443df8f179624bfe4fd88dde51a35aa0bf3a8177
忠庭 蔡 <tom@m.maxpower.com.tw>
2021-08-04 14:02:10 +0800

    @建立 Core版 SignalR

---
+++ ServiceCore/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace ServiceCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var _Con = new HubConnectionBuilder().WithUrl($"https://localhost:44345/hub/BroadcastHub")
+                 .WithAutomaticReconnect(new[] { TimeSpan.Zero, TimeSpan.Zero, TimeSpan.FromSeconds(10) })
+                 .Build();
+
+            //_Con.Closed += async (error) =>
+            //{
+            //    await Task.Delay(new Random().Next(0, 5) * 1000);
+            //    await _Con.StartAsync();
+            //};
+
+            _Con.On("showmessage", (string name, string message) =>
+            {
+                Console.WriteLine($"{name}, {message}\r\n");
+            });
+            _Con.StartAsync().Wait();
+            Console.Read();
+
+        }
+
+    }
+}
---
+++ SignalRCoreTest/Hubs/BroadcastHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRCoreTest.Hubs
+{
+    public class BroadcastHub :Hub
+    {
+
+        public async Task Broadcast(string name, string message)
+        {
+           await Clients.All.SendAsync("showmessage",name, message);
+        }
+    }
+}
